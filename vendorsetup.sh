d18d03008d0e647dc54bea3e516f95388e862bf1
userariii <abhattacharjee717@gmail.com>
2023-11-17 16:44:08 +0000

    miatoll: Track ViPER4AndroidFX repo
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,9 @@ git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_x
 git clone -b miuicam https://gitlab.com/userariii/vendor-xiaomi-miuicamera.git vendor/xiaomi/miuicamera --depth=1
 git clone -b main https://github.com/userariii/hardware_dolby.git hardware/dolby --depth=1
 
+# package/apps
+git clone -b 13.0 https://github.com/userariii/android_packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX --depth=1
+
 # clang
 git clone -b clang-r498229b https://github.com/Xiaomi-SD720G-Devices/AOSP-clang.git prebuilts/clang/host/linux-x86/clang-r498229b --depth=1
 
