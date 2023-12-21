100ecd3444da102412eed617b2ac1bdf9e789891
MaheshTechnicals <msvarma10001@gmail.com>
2023-12-21 04:22:42 +0000

    change kt url

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # device/xiaomi
-git clone -b tiramisu+FBEv2 https://github.com/Xiaomi-SD720G-Devices/android_device_xiaomi_sm6250-common.git device/xiaomi/sm6250-common --depth=1
+git clone -b tiramisu+FBEv2 https://github.com/MaheshTechnicals/device_xiaomi_sm6250-common.git device/xiaomi/sm6250-common --depth=1
 
 # vendor/xiaomi
 git clone -b tiramisu+ https://github.com/Xiaomi-SD720G-Devices/android_vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
@@ -14,4 +14,4 @@ git clone -b 13.0 https://github.com/userariii/android_packages_apps_ViPER4Andro
 git clone -b clang-r498229b https://github.com/Xiaomi-SD720G-Devices/AOSP-clang.git prebuilts/clang/host/linux-x86/clang-r498229b --depth=1
 
 # kernel/xiaomi
-git clone -b RedCherry+ https://github.com/Xiaomi-SD720G-Devices/android_kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
+git clone -b kinesis https://github.com/clarencelol/kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
